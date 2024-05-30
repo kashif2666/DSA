e83aa6732203eb5082b5955b2c46075e22ad2d47
kashif2666 <kashifjsr7@gmail.com>
2024-05-30 23:56:41 +0530

    Sorting Algorithm

---
+++ 05_Sorting/Q2.java
@@ -0,0 +1,36 @@
+// selection sort
+
+import java.util.Arrays;
+
+public class Q2 {
+  public static void main(String[] args) {
+    int arr[] = { -32, 25, -85, 0, 5, 4, 3, 2, 1 };
+    selection(arr);
+    System.out.println(Arrays.toString(arr));
+  }
+
+  static void selection(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      // find the max in the remaining array and then swap with correct index
+      int last = arr.length - i - 1;
+      int maxIndex = getMaxIndex(arr, 0, last);
+      swap(arr, maxIndex, last);
+    }
+  }
+
+  static void swap(int[] arr, int first, int second) {
+    int temp = arr[first];
+    arr[first] = arr[second];
+    arr[second] = temp;
+  }
+
+  static int getMaxIndex(int[] arr, int start, int end) {
+    int max = start;
+    for (int i = start; i <= end; i++) {
+      if (arr[max] < arr[i]) {
+        max = i;
+      }
+    }
+    return max;
+  }
+}
